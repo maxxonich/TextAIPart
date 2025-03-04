de15906d4fcccb26d9934a928cd766fe7bad1f6c
maksym.yevchuk <coolcool1978max@gmail.com>
2025-03-04 14:42:28 +0200

    update

--- orm_models.py
+++ orm_models.py
@@ -8,6 +8,8 @@ class QueryResult(Base):
     __tablename__ = "query_results"
     id = Column(String, primary_key=True, index=True)
     original_text = Column(Text, nullable=False)
-    sentiment = Column(Text, nullable=True)
-    pro_russian_text = Column(Text, nullable=True)
-    categories = Column(Text, nullable=True)
+    sentiment_audio = Column(Text, nullable=True)
+    sentiment_ocr = Column(Text, nullable=True)
+    sentiment_comments = Column(Text, nullable=True)
+    categories_audio = Column(Text, nullable=True)
+    categories_ocr = Column(Text, nullable=True)
